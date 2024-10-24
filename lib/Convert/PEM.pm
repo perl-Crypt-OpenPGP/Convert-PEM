d185bd7e0cf009c18b25117ef497e649bb3abd06
Paul Howarth <paul@city-fan.org>
2024-10-24 11:17:35 +0100

    Avoid warnings about undeclared InForm and OutForm
    Fixes these warnings in Crypt-DSA:
    Use of uninitialized value $param{"InForm"} in uc at /.../Convert/PEM.pm line 53.
    Use of uninitialized value $param{"OutForm"} in uc at /...//Convert/PEM.pm line 57.

--- lib/Convert/PEM.pm
+++ lib/Convert/PEM.pm
@@ -50,11 +50,11 @@ sub _getform {
     my $pem = shift;
     my %param = @_;
 
-    my $in = uc($param{InForm}) || 'PEM';
+    my $in = uc($param{InForm} || 'PEM');
     $in =~ m/^(PEM|DER)$/ or return $pem->error("Invalid InForm '$in': must be PEM or DER");
     $pem->{InForm} = $in;
 
-    my $out = uc($param{OutForm}) || 'PEM';
+    my $out = uc($param{OutForm} || 'PEM');
     $out =~ m/^(PEM|DER)$/ or return $pem->error("Invalid OutForm '$out': must be PEM or DER");
     $pem->{OutForm} = $out;
     $pem;
